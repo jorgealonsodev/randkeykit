cfa733fe9ed2f3cd68101edc93aaf5bf29fe7ca8
Jorge Alonso <j.alonso@enfoquestic.com>
2026-06-19 22:41:40 +0200

    feat(data): add EFF-style wordlist with 100 sorted words

---
+++ src/data/words.js
@@ -0,0 +1,30 @@
+/**
+ * EFF-style wordlist for passphrase generation.
+ *
+ * This is a starter list of 100 short, easy-to-type, alphabetically-sorted words.
+ * TODO: replace with full 7776-word EFF wordlist.
+ * Reference: https://www.eff.org/dice
+ */
+
+export const WORDS = [
+  "acid", "acre", "acts", "afar", "aged",
+  "aids", "aims", "ally", "arch", "arms",
+  "army", "away", "axis", "bake", "bald",
+  "ball", "band", "bank", "bare", "barn",
+  "base", "bath", "bats", "beam", "bean",
+  "bear", "beat", "beef", "beer", "bell",
+  "belt", "bend", "bike", "bill", "bind",
+  "bird", "bite", "blow", "blue", "bomb",
+  "bond", "bone", "book", "boot", "born",
+  "boss", "bowl", "bulk", "burn", "bush",
+  "cafe", "cage", "cake", "calm", "came",
+  "camp", "cape", "card", "care", "cash",
+  "cast", "cave", "cell", "chat", "chip",
+  "clay", "clip", "club", "clue", "coal",
+  "coat", "code", "coin", "cold", "come",
+  "cook", "cool", "cope", "copy", "core",
+  "corn", "cost", "crew", "crop", "cure",
+  "damp", "dare", "dark", "data", "date",
+  "dawn", "dead", "deal", "dear", "debt",
+  "deck", "deep", "deer", "desk", "dial"
+];
